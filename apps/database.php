4ef13982dd44cc14a2e3b17c38d7120b14979433
Anthony <silvestrejao@gmail.com>
2020-03-04 13:48:21 +0800

    added database class

---
+++ apps/database.php
@@ -0,0 +1,20 @@
+<?php
+
+    require_once    __DIR__ . '/MysqliDb.php';
+
+    class   DbConnect   {
+
+        public  $connection, $db;
+
+        function    __construct(){
+
+            $this->db  =   new mysqli("localhost","root","","inventory");
+            // $this->db  =   new mysqli("localhost","root","","inventory");
+
+            $this->connection   =   new MysqliDb($this->db);
+
+        }
+
+    }
+
+?>
